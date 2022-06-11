83360a8edf4c59bf91926a8a7fc28adc074d31e3
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2022-06-11 11:48:08 +0300

    add images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import { lazy, Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
+// import { lazy, Suspense } from 'react';
+// import { Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 
 function App() {
